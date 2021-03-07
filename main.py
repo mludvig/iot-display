c3ebaaac16b9eff749b25ddf8c92bef9494532f9
Michael Ludvig <mludvig@logix.net.nz>
2021-03-07 19:34:24 +1300

    Renamed blinker.py to main.py

--- main.py
+++ main.py
@@ -1,48 +1,90 @@
 #!/usr/bin/env python3
 
-import sys
-import os
 import time
-import logging
 
-from PIL import Image, ImageOps
+from threading import Thread, Timer, Event
 
-from lib import epd2in13d
-from lib.image_tools import get_colour_mask, image_to_epd_data
+import gpiozero
 
-picdir="img"
+from display import Display
 
-#Set output log level
-logging.basicConfig(level=logging.DEBUG)
+LED1 = "GPIO20"
+LED2 = "GPIO21"
+BUTTON = "GPIO16"
 
-RED=(255, 0, 0)
-WHITE=(255, 255, 255)
-BLACK=(0, 0, 0)
+class Blinker(Thread):
+    SLOW = 0.5
+    FAST = 0.1
 
-try:
-    logging.info("epd2in13d Demo")
+    def __init__(self, led1, led2):
+        super(Blinker, self).__init__(name="Blinker")
+        self.led1 = gpiozero.LED(led1)
+        self.led2 = gpiozero.LED(led2)
+        self.set_period(Blinker.SLOW)
 
-    epd = epd2in13d.EPD()
-    logging.info("Init and Clear")
-    epd.init()
-    #epd.Clear(0xFF)
+    def run(self):
+        while True:
+            self.toggle()
+            time.sleep(self.period)
 
-    logging.info("Read Image File")
-    img = Image.open(os.path.join(picdir, 'yes-c.png')).convert('RGB')
-    img_ry = get_colour_mask(img, RED, BLACK, WHITE)
-    img_b = get_colour_mask(img, WHITE, BLACK, WHITE)
-    buf_ry = image_to_epd_data((img_ry))
-    buf_b = image_to_epd_data((img_b))
+    def toggle(self):
+        self.led1.value = not self.led1.value
+        self.led2.value = not self.led1.value
 
-    logging.info("Display image")
-    epd.display(buf_b, buf_ry)
+    def set_period(self, period):
+        self.period = period
+        print(f"Blinker: period set to {period}")
 
-except Exception as e:
-    logging.error(e)
+class Button(Thread):
+    def __init__(self, button, events):
+        super(Button, self).__init__(name="Button")
+        self.button = gpiozero.Button(button)
+        self.events = events
 
-except KeyboardInterrupt:
-    logging.info("ctrl + c:")
+    def run(self):
+        while True:
+            print("Button: waiting for button press")
+            self.button.wait_for_press()
+            self.events["button_press"].set()
+            self.events["button_press"].clear()
+            self.button.wait_for_release()
 
-finally:
-    epd2in13d.epdconfig.module_exit()
-    exit()
+class Controller(Thread):
+    def __init__(self, blinker, display, events):
+        super(Controller, self).__init__(name="Controller")
+        self.blinker = blinker
+        self.display = display
+        self.events = events
+
+    def run(self):
+        import random
+        while True:
+            print("Controller: waiting for Event.button_press")
+            self.events["button_press"].wait()
+            self.blinker.set_period(self.blinker.FAST)
+            self.blinker.toggle()
+            self.display.display(random.choice(["img/yes.png", "img/nope.png", "img/maybe.png"]))
+            self.blinker.set_period(self.blinker.SLOW)
+        
+if __name__ == "__main__":
+    events = {
+        "button_press": Event(),
+        "display_done": Event(),
+    }
+
+    print("Starting Blinker")
+    blinker = Blinker(LED1, LED2)
+    blinker.start()
+
+    print("Starting Button")
+    button = Button(BUTTON, events)
+    button.start()
+
+    print("Creating Display")
+    display = Display()
+
+    print("Starting Controller")
+    controller = Controller(blinker, display, events)
+    controller.start()
+
+    print("Startup done")
