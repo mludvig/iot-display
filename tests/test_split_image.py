6a184890483c5f20eedf90f001060b634c6525c4
Michael Ludvig <mludvig@logix.net.nz>
2021-04-06 17:16:54 +1200

    Removed eInk stuff

--- tests/test_split_image.py
+++
@@ -1,9 +0,0 @@
-from lib.split_image import get_chunks
-
-def test_get_chunks():
-    lst = list(range(20))
-    chunks = list(get_chunks(lst, 3))
-    assert len(chunks) == 7
-    assert chunks[0] == [ 0, 1, 2 ]
-    assert chunks[-1] == [ 18, 19 ]
-
